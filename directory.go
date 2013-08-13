44e40282f96ed68d1668b22215031b5d667cb613
Andreas Schlapsi <andreas.schlapsi@gmail.com>
2013-08-13 20:42:13 +0200

    Implemented hash generation.

--- directory.go
+++ directory.go
@@ -1,22 +1,30 @@
 package main
 
 import (
+	"crypto/md5"
+	"fmt"
+	"io/ioutil"
 	"os"
 	"path/filepath"
 )
 
 type FileInfo struct {
 	Path string
+	Hash []byte
 }
 
-func walkDirectory(rootDir string) (result []FileInfo) {
+func (fi *FileInfo) GetHashstring() string {
+	return fmt.Sprintf("%x", fi.Hash)
+}
+
+func WalkDirectory(rootDir string) (result []FileInfo) {
 	walker := func(path string, fileInfo os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
 		if !fileInfo.IsDir() {
-			result = append(result, FileInfo{Path: path})
+			result = append(result, newFileInfo(path))
 		}
 
 		return nil
@@ -24,4 +32,28 @@ func walkDirectory(rootDir string) (result []FileInfo) {
 
 	filepath.Walk(rootDir, walker)
 	return
+}
+
+func newFileInfo(path string) FileInfo {
+	return FileInfo{Path: path, Hash: getHash(path)}
+}
+
+var hash = md5.New()
+
+func getHash(path string) []byte {
+	hash.Reset()
+
+	content, err := ioutil.ReadFile(path)
+	if err != nil {
+		fmt.Printf("Error while reading file %s: %s\n", path, err)
+		return nil
+	}
+
+	_, err = hash.Write(content)
+	if err != nil {
+		fmt.Printf("Error while generating hash for file %s: %s\n", path, err)
+		return nil
+	}
+
+	return hash.Sum(nil)
 }
--- directory_test.go
+++ directory_test.go
@@ -29,11 +29,26 @@ func assertEqual(t *testing.T, a, b interface{}) {
 }
 
 func TestWalkDirectory(t *testing.T) {
-	directoryTree := walkDirectory("testdir")
+	directoryTree := WalkDirectory("testdir")
 
 	assertEqual(t, 4, len(directoryTree))
 	assertEqual(t, "testdir/subdir1/a.txt", directoryTree[0].Path)
 	assertEqual(t, "testdir/subdir1/b.txt", directoryTree[1].Path)
 	assertEqual(t, "testdir/subdir2/b.txt", directoryTree[2].Path)
-	assertEqual(t, "testdir/subdir2/c.txt", directoryTree[3].Path)	
+	assertEqual(t, "testdir/subdir2/c.txt", directoryTree[3].Path)
+}
+
+func TestHashCodes(t *testing.T) {
+	directoryTree := WalkDirectory("testdir")
+
+	assertEqual(t, "0cc175b9c0f1b6a831c399e269772661", fmt.Sprintf("%x", directoryTree[0].Hash))
+	assertEqual(t, "92eb5ffee6ae2fec3ad71c777531578f", fmt.Sprintf("%x", directoryTree[1].Hash))
+	assertEqual(t, "92eb5ffee6ae2fec3ad71c777531578f", fmt.Sprintf("%x", directoryTree[2].Hash))
+	assertEqual(t, "4a8a08f09d37b73795649038408b5f33", fmt.Sprintf("%x", directoryTree[3].Hash))
+}
+
+func TestGetHashstring(t *testing.T) {
+	directoryTree := WalkDirectory("testdir")
+
+	assertEqual(t, "0cc175b9c0f1b6a831c399e269772661", directoryTree[0].GetHashstring())
 }
