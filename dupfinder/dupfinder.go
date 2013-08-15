0122a176dbc0d851a80614553c97a235246ec419
Andreas Schlapsi <andreas.schlapsi@gmail.com>
2013-08-15 22:18:21 +0200

    Added statistics and file size is added to the key.

--- dupfinder/dupfinder.go
+++ dupfinder/dupfinder.go
@@ -1,7 +1,10 @@
 package dupfinder
 
+import "strconv"
+
 type Duplicates struct {
 	items []*DuplicateItems
+	stats *Stats
 }
 
 type DuplicateItems struct {
@@ -9,23 +12,36 @@ type DuplicateItems struct {
 	files []*FileInfo
 }
 
+type Stats struct {
+	num_files_total int
+	num_duplicate_files int
+	size_files_total int64
+	size_duplicate_files int64
+}
+
 func NewDuplicates(dirTree *DirTree) *Duplicates {
-	duplicates := &Duplicates{items: make([]*DuplicateItems, 0)}
+	stats := &Stats{}
+	duplicates := &Duplicates{items: make([]*DuplicateItems, 0), stats: stats}
 	fileMap := make(map[string][]*FileInfo)
 
 	for _, fi := range dirTree.files {
+		stats.num_files_total++
+		stats.size_files_total += fi.size
 		hashstr := fi.GetHashstring()
+		fileKey := hashstr + strconv.FormatInt(fi.size, 10)
 
-		files, ok := fileMap[hashstr]
+		files, ok := fileMap[fileKey]
 		if !ok {
 			files = make([]*FileInfo, 0)
-			fileMap[hashstr] = files
+			fileMap[fileKey] = files
 		}
-		fileMap[hashstr] = append(files, fi)
+		fileMap[fileKey] = append(files, fi)
 	}
 
 	for key, fileList := range fileMap {
 		if len(fileList) > 1 {
+			stats.num_duplicate_files += len(fileList)
+			stats.size_duplicate_files += int64(len(fileList)) * fileList[0].size
 			duplicates.items = append(duplicates.items, &DuplicateItems{key: key, files: fileList})
 		}
 	}
@@ -35,4 +51,8 @@ func NewDuplicates(dirTree *DirTree) *Duplicates {
 
 func (dup *Duplicates) Count() int {
 	return len(dup.items)
+}
+
+func (dup *Duplicates) Stats() *Stats {
+	return dup.stats
 }
