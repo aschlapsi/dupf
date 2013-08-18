28a2d08397ea119dcd77c0010ea971a612da90f0
Andreas Schlapsi <andreas.schlapsi@gmail.com>
2013-08-19 00:33:31 +0200

    Main program prints result after searching for duplicates.

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"fmt"
 	"os"
+	"time"
 )
 
 func main() {
@@ -15,6 +16,7 @@ func main() {
 		os.Exit(1)
 	}
 
+	start := time.Now()
 	search := FindDuplicates(args...)
 
 	for {
@@ -26,6 +28,25 @@ func main() {
 		fmt.Printf("\033[1K\rProcessing file %s", filePath)
 	}
 
+	duration := time.Since(start)
+	showResult(search, duration)
+}
+
+func showResult(search *SearchProgress, duration time.Duration) {
+	duplicates := search.Duplicates()
+
+	fmt.Println()
+	fmt.Println()
+	fmt.Printf("Found %d duplicate groups:\n", duplicates.Count())
+	for i, group := range duplicates.Groups() {
+		fmt.Printf("    - Group #%d\n", i + 1)
+		for _, file := range group {
+			fmt.Printf("          - %s\n", file.Path())
+		}
+	}
+	fmt.Println()
+	fmt.Printf("Processed %d files with a total file size of %d bytes in %s.\n", search.FileCount(), search.TotalFileSize(), duration.String())
+	fmt.Printf("Total size of duplicated files: %d bytes\n", duplicates.TotalFileSize())
 	fmt.Println()
 }
 
