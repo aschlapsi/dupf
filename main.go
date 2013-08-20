04fb484c3be52babbc07ec6853ddb4f879e1e748
Andreas Schlapsi <andreas.schlapsi@gmail.com>
2013-08-20 07:56:17 +0200

    Removed obsolete comment.

--- main.go
+++ main.go
@@ -49,30 +49,3 @@ func showResult(search *SearchProgress, duration time.Duration) {
 	fmt.Printf("Total size of duplicated files: %d bytes\n", duplicates.TotalFileSize())
 	fmt.Println()
 }
-
-
-/*
-	var dirTree *dupfinder.DirTree
-
-	for _, dir := range args {
-		if dirTree == nil {
-			dirTree = dupfinder.NewDirTree(dir)
-		} else {
-			dirTree.AppendDir(dir)
-		}
-	}
-
-	duplicates := dupfinder.NewDuplicates(dirTree)
-	printDuplicateInfo(duplicates)
-}
-
-func printDuplicateInfo(duplicates *dupfinder.Duplicates) {
-	printStats(duplicates.Stats())
-}
-
-func printStats(stats *dupfinder.Stats) {
-	fmt.Printf("Processed %d files and found %d duplicates.\n", stats.TotalFiles(), stats.DuplicateFiles())
-	fmt.Printf("The total file size was %d bytes and the duplicate files had a size of %d bytes.\n", stats.TotalFileSize(), stats.DuplicateFilesSize())
-	fmt.Printf("Without duplicates the total file size would be %d bytes.\n", stats.TotalFileSizeWithoutDuplicates())
-}
-*/
